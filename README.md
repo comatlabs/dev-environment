30853c9dcab8e4da18163670a23ae4de8cd86c4d
Mathew Comeau <comatlabs@gmail.com>
2020-12-15 23:29:41 -0500

    incremental

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # dev-environment
+
+This repo defines an Ubuntu 18.04 LTS (Bionic) VM development environment.
+Canonical's Multipass will bring up the VM and configure it with cloud-init.
+There are a couple steps outside Multipass to support hyper-v enhanced session mode.
+It should work fine with Multipass on other non-Windows platforms.
--- launch.sh
+++ launch.sh
@@ -1,1 +1,31 @@
-multipass launch -c 6 -d 50G -m 4G --name dev --cloud-init cloud-config.yaml
+multipass launch -c 6 -d 50G -m 4G --name dev --cloud-init cloud-config.yaml > >(tee -a /tmp/multipass.log) 2> >(tee -a /tmp/multipass.log >&2)
+if [ $? ]; then
+  if grep timed ./multipass.log; then
+    echo "Initialization often takes longer than multipass allows..."
+  else
+    echo "Multipass failed.  Quitting."
+    exit 1
+  fi
+fi
+
+wait_for_shutdown (){
+  while ! powershell Get-Vm -Name dev | grep 'Off'
+    do
+      echo "VM still running, retry in 5s"
+      sleep 5
+    done
+}
+
+echo "Waiting 5m for VM to halt."
+export -f wait_for_shutdown
+timeout 5m bash -c wait_for_shutdown
+
+if powershell Get-Vm --Name dev | grep Off; then
+  echo "Timed out waiting for VM to halt. Quitting."
+  exit 1
+else
+  echo "Configuring VM."
+  powershell set-vm dev -enhancedsessiontransporttype hvsocket
+  powershell Enable-VMIntegrationService -VMName dev -Name \"Guest Service Interface\"
+  echo "All set."
+fi
